a6854b6e62724cfb702656d879007a733eebc7a3
bashnia007 <alexanderkavokin@gmail.com>
2021-07-14 23:45:56 +0200

    Added King logic

--- OnlineChess/Assets/Scripts/ChessPieces/King.cs
+++ OnlineChess/Assets/Scripts/ChessPieces/King.cs
@@ -1,7 +1,33 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class King : ChessPiece
 {
+    public override List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] board, int tileCountX, int tileCountY)
+    {
+        var result = new List<Vector2Int>();
+
+        List<Vector2Int> moves = new List<Vector2Int>();
+        moves.Add(new Vector2Int(1, 1));
+        moves.Add(new Vector2Int(1, 0));
+        moves.Add(new Vector2Int(1, -1));
+        moves.Add(new Vector2Int(0, 1));
+        moves.Add(new Vector2Int(0, -1));
+        moves.Add(new Vector2Int(-1, 1));
+        moves.Add(new Vector2Int(-1, 0));
+        moves.Add(new Vector2Int(-1, -1));
+
+        foreach (var move in moves)
+        {
+            int x = currentX + move.x;
+            int y = currentY + move.y;
+
+            if (IsOnBoard(x, y, tileCountX, tileCountY) && (board[x, y] == null || board[x, y].team != team))
+            {
+                result.Add(new Vector2Int(x, y));
+            }
+        }
+
+        return result;
+    }
 }
